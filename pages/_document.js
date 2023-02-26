9b1ac930c7f18a1fc7d7e7e1d310895a5fbe6d01
Thiago Sullivan <thiagosullivan12@gmail.com>
2023-02-26 05:33:41 -0300

    Initializing project

--- pages/_document.js
+++ pages/_document.js
@@ -2,8 +2,27 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="pt-BR">
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200;300;400;800&display=swap" rel="stylesheet"></link>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        {/* Primary Meta Tags */}
+        <meta name="description" content="Surto - Marketing Inteligente" />
+        {/* Open Graph / Facebook */}
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://www.nemeadvogados.adv.br/" />
+        <meta property="og:description" content="Surto - Marketing Inteligente" />
+        <meta property="og:image" content="https://i.ibb.co/ZxQ1JYf/neme-tag-img.jpg" />
+        {/* Twitter */} 
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://www.nemeadvogados.adv.br/" />
+        <meta property="twitter:description" content="Surto - Marketing Inteligente" />
+        <meta property="twitter:image" content="https://i.ibb.co/ZxQ1JYf/neme-tag-img.jpg"></meta>
+      </Head>
       <body>
         <Main />
         <NextScript />
---
+++ styles/theme.js
@@ -0,0 +1,14 @@
+const theme = {
+  primary: '#F7DD46',
+  secondary: '#2E2F33',
+  third: '#292929',
+  gradient: 'linear-gradient(225deg, #313860 0%, #11172B 100%);',
+  grey: '#F5F5F5',
+  greysecond: '#E5E5E5',
+  greythird: '#909090',
+  white: '#FFFFFF',
+  error: '#D45D5D',
+  success: '#6fd45d'
+};
+
+export default theme;
